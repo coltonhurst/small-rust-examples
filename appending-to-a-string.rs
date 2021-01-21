4246dcd6e7552b799460411328e5513c84b07bce
Colton Hurst <colton@coltonhurst.com>
2021-01-20 20:11:20 -0500

    Add comments to appending-to-a-string.rs

--- appending-to-a-string.rs
+++ appending-to-a-string.rs
@@ -1,3 +1,16 @@
+/*
+    Explanation:
+
+    In this example, we are creating a mutable String named my_string.
+    We print my_string, then append an 'a' to the string with the add_an_a() function.
+    Then we print my_string again.
+
+    Extra notes:
+
+    "happy" is a string slice. The to_string method converts the string slice to a string.
+    println!() is a macro. In the examples below we print the value in my_string to the console.
+*/
+
 fn main() {
     let mut my_string: String = "happy".to_string();
 
@@ -8,7 +21,18 @@ fn main() {
     println!("{}", my_string);
 }
 
+
+/*
+    Explanation:
+
+    This function accepts a mutable String reference as a parameter.
+    It "borrows" the String str and appends an 'a' to the end with push_str().
+
+    Extra notes:
+
+    Here, push_str() appends a string slice on the String str.
+    Because "a" here is a string literal, it's a &str (string slice reference).
+*/
 fn add_an_a(str: &mut String) {
     str.push_str("a");
 }
-
